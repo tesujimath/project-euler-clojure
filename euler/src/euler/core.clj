fe6e4ceedf93df658cce0d5d909e3b6478298158
Simon Guest <simon.guest@tesujimath.org>
2025-01-11 16:28:16 +1300

    Use some-fn and partial for e01

--- euler/src/euler/core.clj
+++ euler/src/euler/core.clj
@@ -1,11 +1,16 @@
 (ns euler.core
   (:gen-class))
 
+(defn multiple? [n m]
+  "Is `m` a multiple of `n`?"
+  (zero? (rem m n))
+  )
+
 (defn e01-multiples-of-3-or-5 []
   "the sum of all the multiples of 3 or 5 below 1000"
-  (defn my-multiple? [x] (or (zero? (rem x 3))
-                             (zero? (rem x 5))))
-  (reduce + (filter my-multiple? (range 1 1000))))
+  (reduce + (filter (some-fn (partial multiple? 3)
+                             (partial multiple? 5))
+                    (range 1 1000))))
 
 (defn fib
   "Lazy sequence of Fibonacci numbers."
